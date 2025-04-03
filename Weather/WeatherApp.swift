6f3876e3ca60e17784b2f87c2bac28d1ee394409
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2025-04-03 22:39:37 +0200

    init

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Ahmed Shawky on 03/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Ahmed Shawky on 03/04/2025.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
