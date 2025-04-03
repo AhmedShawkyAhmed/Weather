de73dc60a3470860fe247bcdaba2c73e3d8bd605
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2025-04-03 23:16:32 +0200

    home view

---
+++ Weather/Views/Main/HomeView.swift
@@ -0,0 +1,67 @@
+//
+//  HomeView.swift
+//  Weather
+//
+//  Created by Ahmed Shawky on 03/04/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.backgroundGrad
+                .ignoresSafeArea()
+            
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            Image("House")
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding(.top, 257)
+            
+            VStack {
+                Text("Cairo")
+                    .font(.largeTitle)
+                
+                VStack {
+                    
+                    Text(attributedString)
+                    
+                    Text("H:24°   L:18°")
+                        .font(.title3.weight(.semibold))
+                }
+                
+                Spacer()
+            }
+            .padding(.top, 51)
+        }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostily Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostily Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string
+    }
+}
+
+#Preview {
+    HomeView()
+        .preferredColorScheme(.dark)
+}
